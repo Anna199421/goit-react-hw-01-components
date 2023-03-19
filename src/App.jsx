41ee0c364ef15725be523b3351304491240e9670
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-03-19 15:53:43 +0200

    ggg

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+import Profile from 'components/profile/Profile';
+import Statistics from 'components/statistics/Statistics';
+import FriendList from 'components/friends/FriendList';
+import TransactionHistory from 'components/transactions/TransactionHistory';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
