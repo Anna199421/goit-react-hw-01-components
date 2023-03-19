15cb9b592a4da4d06936ebdb45f919cfbcdfd4b6
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-03-19 21:09:21 +0200

    Update Statistics.jsx
    hhh

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 import { getRandomHexColor } from 'utils';
 import css from './Statistics.module.css';
 
@@ -25,3 +25,13 @@ export default function Statistics({ title = '', stats }) {
     </section>
   );
 }
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
